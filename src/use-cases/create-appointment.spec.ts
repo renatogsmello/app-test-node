ce7505972d110d11b7a05fe8e951fc2d20328f2b
renato.mello <renato.mello@globalhitss.com.br>
2023-02-01 12:15:33 -0300

    create appointment test

---
+++ src/use-cases/create-appointment.spec.ts
@@ -0,0 +1,22 @@
+import { describe, expect, it } from "vitest"
+import { CreateAppointment } from "./create-appointment"
+import { Appointment } from "../entities/appointment"
+
+describe("Create Appointment", () => {
+	it("should be able to create an appointment", () => {
+		const createAppointment = new CreateAppointment()
+
+		const startsAt = new Date()
+		const endsAt = new Date()
+		startsAt.setDate(startsAt.getDate() + 1)
+		endsAt.setDate(endsAt.getDate() + 2)
+
+		expect(
+			createAppointment.execute({
+				customer: "John Doe",
+				startsAt,
+				endsAt,
+			})
+		).resolves.toBeInstanceOf(Appointment)
+	})
+})
